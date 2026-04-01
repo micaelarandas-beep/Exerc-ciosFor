87675d696328f7f1ca739ed859b454671753e4d4
Micael <micael.arandas@gmail.com>
2026-04-01 12:06:34 -0300

    Add files via upload

---
+++ ExercíciosFor1.java
@@ -0,0 +1,9 @@
+public class ExercíciosFor1 {
+
+    public static void main(String[] args) {
+        int contador;
+        for (contador = 100; contador >= 1; contador -= 1){
+            System.out.println(contador);
+        }
+    }
+}
