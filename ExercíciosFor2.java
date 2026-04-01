94e96148958a0a02b787cc52a91bc3ab0f82e1c0
Micael <micael.arandas@gmail.com>
2026-04-01 13:42:43 -0300

    Add files via upload

---
+++ ExercíciosFor2.java
@@ -0,0 +1,11 @@
+public class ExercíciosFor2 {
+
+    public static void main(String[] args) {
+        
+        
+        for(int RaizQ = 15 ; RaizQ < 101; RaizQ ++){
+            double raiz = Math.sqrt(RaizQ);
+            System.out.println(RaizQ + " = "+ raiz);
+        }
+    }
+}
